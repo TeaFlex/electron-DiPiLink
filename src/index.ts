ff9dcb51aadd76d2b519b5b264b9a55915497a9d
Teaflex <teaflex.dev@hotmail.com>
2021-06-01 00:10:59 +0200

    First working version

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { app, BrowserWindow, shell } from 'electron';
+import { join } from "path";
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        minWidth: 400,
+        minHeight: 600, 
+        autoHideMenuBar: true,
+        roundedCorners: true,
+        webPreferences:{
+            devTools: true,
+            preload: join(__dirname, "preload.js")
+        }
+    });
+    
+    win.loadFile('public/build/index.html');
+
+    win.webContents.on("new-window", (e, url) => {
+        shell.openExternal(url);
+        e.preventDefault();
+    });
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    });
+});
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
